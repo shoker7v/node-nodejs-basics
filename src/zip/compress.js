9e65c8bdb7c7612da606fd479ac2fdd0973764d2
Vladimir Burmistrov <thereddevil18@gmail.com>
2022-12-05 01:47:52 +0300

    change file in zlib task

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,12 @@
+import { createReadStream, createWriteStream } from 'fs';
+import { createGzip } from 'zlib';
+
 const compress = async () => {
-    // Write your code here 
+    let readStream = createReadStream('./files/fileToCompress.txt');
+    let writeStream = createWriteStream('./files/archive.gz');
+    let compressStream = createGzip();
+
+    readStream.pipe(compressStream).pipe(writeStream);
 };
 
 await compress();
