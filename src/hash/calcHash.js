df25f90336dbf59420195776a9c168d0ebdd06fa
Vladimir Burmistrov <thereddevil18@gmail.com>
2022-12-04 23:01:43 +0300

    change file in hash task

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,12 @@
-const calculateHash = async () => {
-    // Write your code here 
+import { createHash } from 'crypto';
+import { read } from '../fs/read.js';
+
+const calculateHash = async (file) => {
+    let content = await read(file);
+    function sha256(content) {  
+        return createHash('sha3-256').update(content).digest('hex')
+      }
+    console.log(sha256(content));
 };
 
-await calculateHash();
+await calculateHash('./files/fileToCalculateHashFor.txt');
